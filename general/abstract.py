5ff9b24818c4afcad5fa0c6f793913a6106074ae
Ken Ho <kenhks014@gmail.com>
2020-08-24 09:57:18 +0800

    enhance base abstract model Add two field create_user and write_user to track record activity, add verbose name for field description

--- general/abstract.py
+++ general/abstract.py
@@ -1,17 +1,26 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.conf import settings
 from django.utils.translation import gettext as _
+
 
 class BaseAbstractModel(models.Model):
     """
     Abstract Base Model
     """
-    create_date = models.DateTimeField(auto_now_add=True, verbose_name=_("Created on"))
-    write_date = models.DateTimeField(auto_now=True, verbose_name=_("Last Modified on"))
+    create_date = models.DateTimeField(
+        auto_now_add=True, verbose_name=_("Created on"))
+    create_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT, verbose_name=_("Created by"))
+    write_date = models.DateTimeField(
+        auto_now=True, verbose_name=_("Last Modified on"))
+    write_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT, verbose_name=_("Last Modified by"))
 
     class Meta:
         abstract = True
         get_latest_by = "write_date"
+
 
 class ActiveControlMixin(models.Model):
     """
