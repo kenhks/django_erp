7256e4f028dbaba6466749abec428a34d68badac
Ken Ho <kenhks014@gmail.com>
2020-12-01 19:21:59 +0800

    add package django-import-export in all development environment

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
     'rest_framework',
     'django_filters',
     'base',
